c6f35c564012e5931187bff9ba66eb986a765f6d
xqnstk <xqnstk@gmail.com>
2018-05-31 18:29:57 +0900

    api 4개 연결

--- js/WebContent/project_api/property.js
+++ js/WebContent/project_api/property.js
@@ -199,31 +199,45 @@ function displayMarker(locPosition, message) {
 function handleRefresh() {
 	var url="http://openapi.seoul.go.kr:8088/4a6a63497978716e36365544455967/json/octastatapi419/"+gus+"/"+gus;
 	var url2="http://openapi.seoul.go.kr:8088/4c4b71576578716e363065654a414b/json/octastatapi10164/"+gus+"/"+gus;
+	var url3="http://openapi.seoul.go.kr:8088/796144645878716e3530664c4d5563/json/octastatapi10057/"+gus+"/"+gus;
+	var url4="http://openapi.seoul.go.kr:8088/794673545878716e3637635a516868/json/octastatapi37/"+gus+"/"+gus;
 
 	$.getJSON(url, updateLibrary);
 	$.getJSON(url2, draw);
-
-	addBound();// 지도에 원을 표시
+	$.getJSON(url3, trafficAcc);
+	$.getJSON(url4, diseaseAcc);
+	
 	//리스트 부분
     var newScriptElement = document.createElement("script");
 	newScriptElement.setAttribute("src", url);
 	var SCElement = document.createElement("script");
 	SCElement.setAttribute("src", url2);
+	var Element = document.createElement("script");
+	Element.setAttribute("src", url3);
+	var element = document.createElement("script");
+	element.setAttribute("src", url4);
 	
 	/*jsonp를 사용하여 스크립트 정보를 갱신*/
 	newScriptElement.setAttribute("id", "jsonp");
 	SCElement.setAttribute("id", "jsonp");
+	Element.setAttribute("id", "jsonp");
+	element.setAttribute("id", "jsonp");
 
 	var oldScriptElement = document.getElementById("jsonp");
 	var head = document.getElementsByTagName("head")[0];
-	
+	addBound();// 지도에 원을 표시
+
 	if(oldScriptElement == null){
 		head.appendChild(newScriptElement);
 		head.appendChild(SCElement);
+		head.appendChild(Element);
+		head.appendChild(element);
 	}
 	else{
 		head.replaceChild(newScriptElement, oldScriptElement);
 		head.replaceChild(SCElement, oldScriptElement);
+		head.replaceChild(Element, oldScriptElement);
+		head.replaceChild(element, oldScriptElement);
 	}
 
 }//handleRefresh
@@ -287,6 +301,7 @@ function updateLibrary(librarys) {
 			if (lib.GYE_3 != "") {
 				div.innerHTML += "<br>" + "외국인 거주자 : " + lib.GYE_3 + "명";
 			}
+			
 			if(librarysDiv.childElementCount==0){
 				librarysDiv.appendChild(div);
 			}
@@ -349,11 +364,14 @@ function draw(cfires) {
 			
 			if (aaa == gus) {
 		
-				if (lib.GYE_2 != "") {
+				if (lib.GYE_1 != "") {
 					div.innerHTML += "<br>" + "화재 사건 수 : " + lib.HAPGYE_1 +"회";
 				}
 				if (lib.GYE_3 != "") {
 					div.innerHTML += "<br>" + "인명피해 : " + lib.HAPGYE_3 + "명";
+				}
+				if (lib.JAESANPIHAEGYEONGGAMAEK != "") {
+					div.innerHTML += "<br>" + "재산피해 경감액 : " + lib.JAESANPIHAEGYEONGGAMAEK + "원";
 				}
 				if(cfiresDiv.childElementCount==0){
 					cfiresDiv.appendChild(div);
@@ -369,6 +387,152 @@ function draw(cfires) {
 			lastReportTime = cfires[cfires.length-1].time;
 		}
 }//draw()
+
+function trafficAcc(ctraffic) {
+	   var ctraffic = ctraffic.octastatapi10057.row;
+	   var addr = "";
+	   var center = map.getCenter(); // 중심 가져오기 
+	   var position = {
+	          latitude : center.getLat(),
+			  longitude: center.getLng()
+	      };
+	   
+	   for (var i = 0; i < ctraffic.length; i++) {
+	      var lib = ctraffic[i];
+	      var imageSrc = "marker1.png",
+			imageSize = new daum.maps.Size(27, 40), //마커의 크기(daummap에서 size 검색, 크기정보를 가지고 있는 사이즈 객체 생성)
+			imageOption = {offset: new daum.maps.Point(14, 28)};//point 검색, 화면 좌표 정보를 담고 있는 포인터 객체 생성
+		  
+	      var loc = {//open API의 값들 위도와 경도
+	            latitude : lib.XCNTS,
+				longitude: lib.YDNTS
+	      }; 
+	   	}
+	   
+	   //밑에 리스트 추가하는 부분
+	   	var ctrafficDiv = document.getElementById("traffic");
+		ctrafficDiv.innerHTML="";
+		
+		for(var i=0; i<ctraffic.length; i++){
+			var lib = ctraffic[i];
+			var div = document.createElement("div");
+			div.setAttribute("class", "ctraffic");
+			var aaa;
+			if(gus == 2)	aaa= 2;
+			else if(gus == 3) 	aaa=3;
+			else if(gus == 4) 	aaa=4;
+			else if(gus == 5) 	aaa=5;
+			else if(gus == 6) 	aaa=6;
+			else if(gus == 7) 	aaa=7;
+			else if(gus == 8) 	aaa=8;
+			else if(gus == 9) 	aaa=9;
+			else if(gus == 10) 	aaa=10;
+			else if(gus == 11) 	aaa=11;
+			else if(gus == 12) 	aaa=12;
+			else if(gus == 13) 	aaa=13;
+			else if(gus == 14) 	aaa=14;
+			else if(gus == 15) 	aaa=15;
+			else if(gus == 16) 	aaa=16;		
+			
+			if (aaa == gus) {
+		
+				if (lib.BALSAENGGEONSU != "") {
+					div.innerHTML += "<br>" + "발생건수 : " + lib.BALSAENGGEONSU +"회";
+				}
+				if (lib.SAMANGJASU != "") {
+					div.innerHTML += "<br>" + "사망자수 : " + lib.SAMANGJASU + "명";
+				}
+				if (lib.BUSANGJASU != "") {
+					div.innerHTML += "<br>" + "부상자수 : " + lib.BUSANGJASU + "명";
+				}
+				if(ctrafficDiv.childElementCount==0){
+					ctrafficDiv.appendChild(div);
+				}
+				else{
+					ctrafficDiv.insertBefore(div, ctrafficDiv.firstChild);
+				}
+			} //if
+			
+		}//for
+
+		if(ctraffic.length > 0){
+			lastReportTime = ctraffic[ctraffic.length-1].time;
+		}
+}//trafficAcc()
+
+
+function diseaseAcc(cdisease) {
+	   var cdisease = cdisease.octastatapi37.row;
+	   var addr = "";
+	   var center = map.getCenter(); // 중심 가져오기 
+	   var position = {
+	          latitude : center.getLat(),
+			  longitude: center.getLng()
+	      };
+	   
+	   for (var i = 0; i < cdisease.length; i++) {
+	      var lib = cdisease[i];
+	      var imageSrc = "marker1.png",
+			imageSize = new daum.maps.Size(27, 40), //마커의 크기(daummap에서 size 검색, 크기정보를 가지고 있는 사이즈 객체 생성)
+			imageOption = {offset: new daum.maps.Point(14, 28)};//point 검색, 화면 좌표 정보를 담고 있는 포인터 객체 생성
+		  
+	      var loc = {//open API의 값들 위도와 경도
+	            latitude : lib.XCNTS,
+				longitude: lib.YDNTS
+	      }; 
+	   	}
+	   
+	   //밑에 리스트 추가하는 부분
+	   	var cdiseaseDiv = document.getElementById("disease");
+		cdiseaseDiv.innerHTML="";
+		
+		for(var i=0; i<cdisease.length; i++){
+			var lib = cdisease[i];
+			var div = document.createElement("div");
+			div.setAttribute("class", "cdisease");
+			var aaa;
+		
+			if (gus == 2) aaa=2;
+			else if(gus == 3) 	aaa=3;
+			else if(gus == 4) 	aaa=4;
+			else if(gus == 5) 	aaa=5;
+			else if(gus == 6) 	aaa=6;
+			else if(gus == 7) 	aaa=7;
+			else if(gus == 8) 	aaa=8;
+			else if(gus == 9) 	aaa=9;
+			else if(gus == 10) 	aaa=10;
+			else if(gus == 11) 	aaa=11;
+			else if(gus == 12) 	aaa=12;
+			else if(gus == 13) 	aaa=13;
+			else if(gus == 14) 	aaa=14;
+			else if(gus == 15) 	aaa=15;
+			else if(gus == 16) 	aaa=16;		
+			
+			if (aaa == gus) {
+		
+				if (lib.BALSAENG_1 != "") {
+					div.innerHTML += "<br>" + "1군감염병(장티푸스, A형 감염 ..) : " + lib.BALSAENG_1 +"명";
+				}
+				if (lib.BALSAENG_7 != "") {
+					div.innerHTML += "<br>" + "2군감염병(수두, B형 감염 ..) : " + lib.BALSAENG_7 + "명";
+				}
+				if (lib.BALSAENG_16 != "") {
+					div.innerHTML += "<br>" + "3군감염병(결핵, 쯔쯔가무시증, 말라리아 ..) : " + lib.BALSAENG_16 + "명";
+				}
+				if(cdiseaseDiv.childElementCount==0){
+					cdiseaseDiv.appendChild(div);
+				}
+				else{
+					cdiseaseDiv.insertBefore(div, cdiseaseDiv.firstChild);
+				}
+			} //if
+		}//for
+
+		if(cdisease.length > 0){
+			lastReportTime = cdisease[cdisease.length-1].time;
+		}
+}//diseaseAcc()
+
 
 function addBound(){
 	// 지도에 표시할 원을 생성합니다
@@ -390,23 +554,6 @@ function addBound(){
 	daum.maps.event.addListener(map, 'dragstart', function() {//지도가 이동될때도 원이 다시 그려짐
 		bound.setMap(null);
 	});
-}
-
-
-// 오류찾는 함수
-function displayError(error) {
-	var errorTypes = {
-		0 : "Unknown error",
-		1 : "Permission denied",
-		2 : "Position is not available",
-		3 : "Request timeout"
-	};
-	var errorMessage = errorTypes[error.code];
-	if (error.code == 0 || error.code == 2) {
-		errorMessage = errorMessage + " " + error.message;
-	}
-	var div = document.getElementById("location");
-	div.innerHTML = errorMessage;
 }
 
 
